3fdc98c0d178507f7cb1e6eff494d4655395f993
Martin Sadler <mtsbtt@googlemail.com>
2009-03-07 16:48:44 +0000

    wip

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,22 +1,21 @@
 class CompaniesController < ApplicationController
 
-  def search
-    term = params[:q]
-    
-  end
 
+
+  
   # fuzzy match
-  def index
-    @query = params[:q]
-    @companies = Company.find_all_by_company_name(@query)
-    
+  def search
+     @query  =  params[:q]
+     #@nice_name = name.gsub(/_|-/,' ').titleize
+     @companies = Company.find_all_by_company_name(@query)
+     redirect_to companies_url(@companies.first) if @companies.size == 1
   end
-
+      
   # exact match
   def show
     name  =  params[:path].first if params[:path].instance_of?(Array) 
     @nice_name = name.gsub(/_|-/,' ').titleize
-    @company = Company.find_by_name(name)  
+    @company = Company.find(:first)    
   end
 
 
