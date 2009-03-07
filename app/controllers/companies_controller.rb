32a7d326699da44641103776c57568bfad0b7079
robmckinnon <rob@theyworkforyou.co.nz>
2009-03-07 16:51:41 +0000

    added company show action

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,20 +1,31 @@
 class CompaniesController < ApplicationController
 
+  before_filter :ensure_current_url, :only => :show
+
   def search
     term = params[:q]
-    
+
   end
 
   def index
     @query = params[:q]
     @companies = Company.find_by_company_name(@query)
-  
   end
 
   def show
-    name  =  params[:path].first if params[:path].instance_of?(Array) 
-    @nice_name = name.gsub(/_|-/,' ').titleize
   end
 
+  private
+
+    def ensure_current_url
+      begin
+        unless params[:id].include? '+'
+          @company = Company.find_this(params[:id])
+          redirect_to @company, :status => :moved_permanently if @company.has_better_id?
+        end
+      rescue
+        render_not_found
+      end
+    end
 
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,12 +1,21 @@
 class Company < ActiveRecord::Base
 
+  has_friendly_id :name, :use_slug => true, :strip_diacritics => true
+
   has_many :lobbyist_clients
   has_many :ogc_suppliers
-
 
   class << self
     def find_by_company_name name
       find(:all, :conditions => %Q|name like "%#{name.gsub('"','')}%"|)
+    end
+
+    def find_this identifier
+      company = find_by_company_number(identifier)
+      unless company
+        company = find(identifier)
+      end
+      company
     end
   end
 end
