16fcf443c5dd12c6e5134760e726762ad422f74f
robmckinnon <rob@theyworkforyou.co.nz>
2009-03-08 19:24:28 +0000

    added page showing rewired state presentation video

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,7 @@
 class HomeController < ApplicationController
-
-
   def index
-    
   end
-  
 
+  def rewired_state_presentation
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   map.root :controller => "home"
+  map.rewired_state_presentation '/rewired_state_presentation', :controller => "home", :action => :rewired_state_presentation
 
   map.show_it_formatted '/:id.:format', :conditions => { :method => :get }, :controller => 'companies', :action => 'show'
   map.show_it '/:id', :conditions => { :method => :get }, :controller => 'companies', :action => 'show'
