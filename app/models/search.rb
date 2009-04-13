2bac32bf7dd55f2fdf640010f8a990d039a58a44
robmckinnon <rob@theyworkforyou.co.nz>
2009-04-13 18:24:07 +0100

    set source on has_many through association

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,7 +2,7 @@ class Search < ActiveRecord::Base
 
   has_many :search_results, :dependent => :delete_all
 
-  has_many :companies, :through => :search_results
+  has_many :companies, :through => :search_results, :source => :company
 
   validates_uniqueness_of :term
 
