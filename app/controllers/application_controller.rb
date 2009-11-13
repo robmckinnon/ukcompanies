5e821f762ef6433f2617042b2cd9962cb12b9b4b
robmckinnon <rob@theyworkforyou.co.nz>
2009-11-13 00:39:06 +0000

    redirect old urls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,45 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
+
+  before_filter :redirect_old_urls
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 
   def render_not_found message='Page not found.'
     render :text => message, :status => :not_found
+  end
+
+  def is_ukcompanies_request?
+    request.host == 'ukcompani.es' ||
+        (RAILS_ENV == 'development' && request.host == 'localhost') ||
+        (RAILS_ENV == 'test' && request.host == 'test.host')
+  end
+
+  def is_companiesrevealed_request?
+    request.host == 'companiesrevealed.org'
+  end
+
+  def is_companiesopen_request?
+    request.host == 'companiesopen.org'
+  end
+
+  def redirect_old_urls
+    path = request.path
+    if is_companiesrevealed_request?
+      redirect_to "http://companiesopen.org#{path}"
+    elsif is_ukcompanies_request?
+      if path.starts_with?('/search') || path == '/'
+        redirect_to "http://companiesopen.org#{path}"
+      elsif
+        redirect_to "http://companiesopen.org/uk#{path}"
+      end
+    elsif !is_companiesopen_request?
+      render(:text => 'not found', :status => 404)
+    end
   end
 end
